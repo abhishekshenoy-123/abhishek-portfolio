77c823f11b4e44468da30c8237c15aa76fef80ca
Abhisek-Lcode <abhisheklcode05@gmail.com>
2026-04-22 15:30:30 +0530

    Footer modified

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -124,12 +124,7 @@ const Footer = () => {
           viewport={{ once: true }}
         >
           <div className="flex flex-col md:flex-row justify-between items-center">
-            <p className="text-text/60 text-sm mb-4 md:mb-0">
-              © {currentYear} R Abhishek Shenoy. All rights reserved.
-            </p>
-            <p className="text-text/60 text-sm flex items-center">
-              Made with <FaHeart className="text-red-500 mx-1" /> using React & Tailwind CSS
-            </p>
+            
           </div>
         </motion.div>
       </div>
